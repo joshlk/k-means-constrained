84c388fac2418ac36d8a3a5b059ca13909822eb4
Josh Levy-Kramer <josh@levykramer.co.uk>
2020-06-17 11:43:32 +0100

    Improved cython build process

--- k_means_constrained/mincostflow_vectorized.py
+++ k_means_constrained/mincostflow_vectorized.py
@@ -4,52 +4,21 @@ import numpy as np
 from ortools.graph.pywrapgraph import SimpleMinCostFlow
 
 
-class SimpleMinCostFlowNonVectorized(SimpleMinCostFlow):
+# Cython paths must be fully qualified
+from k_means_constrained.mincostflow_vectorized_ import \
+    SimpleMinCostFlow_AddArcWithCapacityAndUnitCostVectorized, \
+    SimpleMinCostFlow_SetNodeSupplyVectorized, \
+    SimpleMinCostFlow_FlowVectorized
+
+
+class SimpleMinCostFlowVectorized(SimpleMinCostFlow):
 
     def AddArcWithCapacityAndUnitCostVectorized(self, tail, head, capacity, unit_cost):
-        l = tail.shape[0]
-        assert head.shape[0] == l
-        assert capacity.shape[0] == l
-        assert unit_cost.shape[0] == l
-
-        for i in range(l):
-            self.AddArcWithCapacityAndUnitCostVectorized(tail[i], head[i], capacity[i], unit_cost[i])
+        return SimpleMinCostFlow_AddArcWithCapacityAndUnitCostVectorized(self, tail, head, capacity, unit_cost)
 
     def SetNodeSupplyVectorized(self, node, supply):
-        l = node.shape[0]
-        assert len(supply) == l
-
-        for i in range(l):
-            self.SetNodeSupplyVectorized(node[i], supply[i])
+        return SimpleMinCostFlow_SetNodeSupplyVectorized(self, node, supply)
 
     def FlowVectorized(self, arc):
-        l = arc.shape[0]
-        flow = np.zeros(l, dtype=arc.dtype)
+        return SimpleMinCostFlow_FlowVectorized(self, arc)
 
-        for i in range(l):
-            flow[i] = self.FlowVectorized(arc[i])
-
-        return flow
-
-try:
-    # Cython paths must be fully qualified
-    from k_means_constrained.mincostflow_vectorized_ import \
-        SimpleMinCostFlow_AddArcWithCapacityAndUnitCostVectorized, \
-        SimpleMinCostFlow_SetNodeSupplyVectorized, \
-        SimpleMinCostFlow_FlowVectorized
-
-
-    class SimpleMinCostFlowVectorized(SimpleMinCostFlow):
-
-        def AddArcWithCapacityAndUnitCostVectorized(self, tail, head, capacity, unit_cost):
-            return SimpleMinCostFlow_AddArcWithCapacityAndUnitCostVectorized(self, tail, head, capacity, unit_cost)
-
-        def SetNodeSupplyVectorized(self, node, supply):
-            return SimpleMinCostFlow_SetNodeSupplyVectorized(self, node, supply)
-
-        def FlowVectorized(self, arc):
-            return SimpleMinCostFlow_FlowVectorized(self, arc)
-
-except ImportError:
-    # Cython not compiled default to non vectorised version
-    SimpleMinCostFlowVectorized = SimpleMinCostFlowNonVectorized
